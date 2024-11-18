a8a70651d46ce1486550300fd04ee8a12ae50a82
Emil Högberg <emil.hogberg@gmail.com>
2024-11-18 13:13:41 +0100

    Fixed Next Config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
